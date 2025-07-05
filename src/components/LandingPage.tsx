949a8746bf177bb574019c6f49c8624835d7f501
M Ulmer <mju9999@gmail.com>
2025-07-04 19:05:15 -0700

    Add link to shop page from home page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function LandingPage() {
   return (
     <>
-      <p>Home page here</p>
+      <p>Home page</p>
+      <Link to="/shop">Shop</Link>
     </>
   );
 }
