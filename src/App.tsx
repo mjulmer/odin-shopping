09056adcb55ac200c7ed90e53ef8750e22e3c3a9
M Ulmer <mju9999@gmail.com>
2025-07-04 18:59:53 -0700

    Add counter for shopping cart and pipe through to shop page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <>
       <p>Nav bar will go here</p>
-      <Outlet />
+      <p>Nav bar has shopping cart item counter: {itemCount}</p>
+      <Outlet
+        context={[
+          () => setItemCount(itemCount + 1),
+          () => setItemCount(itemCount - 1),
+        ]}
+      />
     </>
   );
 }
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,7 +1,13 @@
+import { useOutletContext } from "react-router-dom";
+
 function Shop() {
+  const [increaseItemCount, decreaseItemCount]: Array<() => void> =
+    useOutletContext();
   return (
     <>
       <p>Shop page here</p>
+      <button onClick={increaseItemCount}>Increase the counter</button>
+      <button onClick={decreaseItemCount}>Decrease the counter</button>
     </>
   );
 }
