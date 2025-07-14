ad9d7858b990a2ebf15553bcb45e621fad311974
M Ulmer <mju9999@gmail.com>
2025-07-14 10:12:17 -0700

    Add basic navbar styling
    This change adds the navbar elements and lays them out in roughly
    the appropriate places, but doesn't yet hook up any of the links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import "./styles.css";
 
 function App() {
   const [itemCount, setItemCount] = useState(0);
   return (
-    <>
-      <p>Nav bar will go here</p>
-      <p>Nav bar has shopping cart item counter: {itemCount}</p>
+    <div className="container">
+      <div className="navbar">
+        <span className="shopLogo">Fake shop</span>
+        <div className="navbarItem">Home</div>
+        <div className="navbarItem">Shop</div>
+        <div className="navbarItem">Cart ({itemCount})</div>
+      </div>
+
       <Outlet
         context={[
           () => setItemCount(itemCount + 1),
           () => setItemCount(itemCount - 1),
         ]}
       />
-    </>
+    </div>
   );
 }
 
