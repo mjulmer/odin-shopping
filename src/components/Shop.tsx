707ec5a0c6a44b40204aa32428bf1f08ed068ba2
M Ulmer <mju9999@gmail.com>
2025-07-15 21:36:33 -0700

    Fix bug where item count object wasn't correctly initialized
    If the user navigated to the shop page before the items had been
    fetched from the external API, the itemValues state would be
    initialized to an empty object, since there were no items to set
    counts for.
    
    Now, a loading screen is shown if there are no items, and the
    item component is only mounted once the items are available.
    Therefore, the itemValues object is initialized with zeros
    associated with each item ID, and React doesn't complain about an
    uncontrolled component becoming controlled.

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,90 +1,19 @@
-import { useState, useMemo } from "react";
 import { useOutletContext } from "react-router-dom";
 import type { ShoppingItem } from "../ShoppingItem";
 import "./shop.css";
+import ShoppingItems from "./ShoppingItems";
 
 function Shop() {
   const {
-    increaseItemCount,
     shoppingItems,
   }: {
-    increaseItemCount: (valueToAdd: number) => void;
     shoppingItems: Array<ShoppingItem>;
   } = useOutletContext();
-
-  // Bug - if initial object is populated before items loaded, the items won't
-  // get correctly initialized.
-  const [itemValues, setItemValues] = useState<{ [itemId: string]: number }>(
-    useMemo(
-      () =>
-        shoppingItems.reduce(
-          (valuesObj, shoppingItem) => ({ ...valuesObj, [shoppingItem.id]: 0 }),
-          {}
-        ),
-      [shoppingItems]
-    )
-  );
-  function onChangeNumberOfItem(newValue: string, eventId: string) {
-    let intValue = parseInt(newValue);
-    // If the user has a valid integer product quantity, then types in
-    // "flingibbit" and then hits "add to cart", no item should be added.
-    if (Number.isNaN(intValue)) {
-      intValue = 0;
-    }
-    if (intValue < 0) {
-      intValue = 0;
-    }
-    setItemValues({ ...itemValues, [eventId]: intValue });
-  }
 
   return (
     <>
       <h1>This is the shop page </h1>
-      <div className="shopItemsContainer">
-        {shoppingItems.map((item) => (
-          <div className="shopItem" key={item.id}>
-            <img src={item.image} alt=""></img>
-            <p>{item.title}</p>
-            <span>
-              <button
-                aria-label="Decrease by one"
-                onClick={() =>
-                  onChangeNumberOfItem(
-                    (itemValues[item.id] - 1).toString(),
-                    item.id
-                  )
-                }
-              >
-                -
-              </button>
-              <input
-                type="integer"
-                onChange={(event) =>
-                  onChangeNumberOfItem(event.target.value, item.id)
-                }
-                value={itemValues[item.id]}
-              ></input>
-              <button
-                aria-label="Increase by one"
-                onClick={() =>
-                  onChangeNumberOfItem(
-                    (itemValues[item.id] + 1).toString(),
-                    item.id
-                  )
-                }
-              >
-                +
-              </button>
-              <button
-                className="addToCart"
-                onClick={() => increaseItemCount(itemValues[item.id])}
-              >
-                Add to cart
-              </button>
-            </span>
-          </div>
-        ))}
-      </div>
+      {shoppingItems.length ? <ShoppingItems /> : <p>Loading...</p>}
     </>
   );
 }
---
+++ src/components/ShoppingItems.tsx
@@ -0,0 +1,88 @@
+import { useState, useMemo } from "react";
+import { useOutletContext } from "react-router-dom";
+import type { ShoppingItem } from "../ShoppingItem";
+import "./shop.css";
+
+function ShoppingItems() {
+  const {
+    increaseItemCount,
+    shoppingItems,
+  }: {
+    increaseItemCount: (valueToAdd: number) => void;
+    shoppingItems: Array<ShoppingItem>;
+  } = useOutletContext();
+
+  const [itemValues, setItemValues] = useState<{ [itemId: string]: number }>(
+    useMemo(
+      () =>
+        shoppingItems.reduce(
+          (valuesObj, item) => ({ ...valuesObj, [item.id]: 0 }),
+          {}
+        ),
+      [shoppingItems]
+    )
+  );
+
+  function onChangeNumberOfItem(newValue: string, eventId: string) {
+    let intValue = parseInt(newValue);
+    // If the user has a valid integer product quantity, then types in
+    // "flingibbit" and then hits "add to cart", no item should be added.
+    if (Number.isNaN(intValue)) {
+      intValue = 0;
+    }
+    if (intValue < 0) {
+      intValue = 0;
+    }
+    setItemValues({ ...itemValues, [eventId]: intValue });
+  }
+
+  return (
+    <div className="shopItemsContainer">
+      {shoppingItems.map((item) => (
+        <div className="shopItem" key={item.id}>
+          <img src={item.image} alt=""></img>
+          <p>{item.title}</p>
+          <span>
+            <button
+              aria-label="Decrease by one"
+              onClick={() =>
+                onChangeNumberOfItem(
+                  (itemValues[item.id] - 1).toString(),
+                  item.id
+                )
+              }
+            >
+              -
+            </button>
+            <input
+              type="integer"
+              onChange={(event) =>
+                onChangeNumberOfItem(event.target.value, item.id)
+              }
+              value={itemValues[item.id]}
+            ></input>
+            <button
+              aria-label="Increase by one"
+              onClick={() =>
+                onChangeNumberOfItem(
+                  (itemValues[item.id] + 1).toString(),
+                  item.id
+                )
+              }
+            >
+              +
+            </button>
+            <button
+              className="addToCart"
+              onClick={() => increaseItemCount(itemValues[item.id])}
+            >
+              Add to cart
+            </button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ShoppingItems;
